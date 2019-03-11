6ff4dbd77916255b832c866e9db48d01db431a4f
asoto01 <mr.anthonygsoto@gmail.com>
2019-03-10 21:16:41 -0700

    Added java files.

---
+++ java/LinkedList/LinkedList.java
@@ -0,0 +1,14 @@
+public class LinkedList {
+	Node head;
+	public static class Node {
+		int data;
+		Node next;
+		Node(int data) {
+			this.data = data;
+		}
+	}
+	// Insert a node method.
+	public LinkedList insert(LinkedList list, int data) {
+	
+	}
+}
